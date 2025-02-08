19331d99cf999a8fdb19fbfc28aa9f322621caa6
pat-nel87 <71235856+pat-nel87@users.noreply.github.com>
2025-02-07 20:08:15 -0500

    Update test.sh

--- flux-secret-test/test.sh
+++ flux-secret-test/test.sh
@@ -11,3 +11,4 @@ kubectl patch deployment <deployment-name> -n cluster-config --patch '{
 kubectl patch deployment my-deployment -n cluster-config --type merge -p '{"spec": {"template": {"spec": {"serviceAccountName": "my-workload-sa"}}}}'
 
 kubectl patch secretproviderclass azure-kv -n cluster-config --type merge -p '{"metadata": {"annotations": {"resync-timestamp": "'$(date +%s)'"}}}'
+kubectl run test-imds --rm -it --image=busybox -- /bin/sh -c "apk add --no-cache curl && curl -H Metadata:true 'http://169.254.169.254/metadata/identity/oauth2/token?api-version=2018-02-01&resource=https://vault.azure.net'"
