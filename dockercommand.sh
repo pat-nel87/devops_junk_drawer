df09dca0acb3ebf1c52ab9aad80a8f1394463411
pat-nel87 <71235856+pat-nel87@users.noreply.github.com>
2025-02-26 13:11:59 -0500

    Update dockercommand.sh

--- dockercommand.sh
+++ dockercommand.sh
@@ -4,3 +4,44 @@ kubectl create secret docker-registry <secret-name> \
   --docker-password=<password> \
   --docker-email=<email> \
   --namespace=<namespace>
+
+
+kubectl patch serviceaccount default -n <namespace> -p \
+  '{"imagePullSecrets": [{"name": "<secret-name>"}]}'
+
+
+
+  apiVersion: v1
+kind: Pod
+metadata:
+  name: my-pod
+  namespace: <namespace>
+spec:
+  containers:
+  - name: my-container
+    image: <registry-url>/<image-name>:<tag>
+  imagePullSecrets:
+  - name: <secret-name>
+
+
+
+apiVersion: apps/v1
+kind: Deployment
+metadata:
+  name: my-deployment
+  namespace: <namespace>
+spec:
+  replicas: 1
+  selector:
+    matchLabels:
+      app: my-app
+  template:
+    metadata:
+      labels:
+        app: my-app
+    spec:
+      containers:
+      - name: my-container
+        image: <registry-url>/<image-name>:<tag>
+      imagePullSecrets:
+      - name: <secret-name>
