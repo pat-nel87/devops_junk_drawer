0fe904e84285c3a2253d4e7a5f8bccd1320b971e
pat-nel87 <71235856+pat-nel87@users.noreply.github.com>
2025-03-30 19:27:53 -0400

    Update dockercommand.sh

--- dockercommand.sh
+++ dockercommand.sh
@@ -51,3 +51,31 @@ docker run --rm \
       -v "${{ github.workspace }}:/workdir" \
       your-acr.azurecr.io/flux-helpers:latest \
       bump --file "/workdir/$FILE" $SET_ARGS --dry-run="$DRY_RUN"
+
+- name: Configure Git
+  run: |
+    git config user.name "${{ github.actor }}"
+    git config user.email "${{ github.actor }}@users.noreply.github.com"
+
+- name: Create new branch and commit changes
+  run: |
+    BRANCH="bump/${{ github.actor }}/$(date +%s)"
+    git checkout -b "$BRANCH"
+    git add "${{ inputs.file }}"
+    git commit -m "chore: bump image versions by @${{ github.actor }}"
+    git push origin "$BRANCH"
+  env:
+    GITHUB_TOKEN: ${{ secrets.GITHUB_TOKEN }}
+
+- name: Open Pull Request
+  uses: peter-evans/create-pull-request@v5
+  with:
+    token: ${{ secrets.GITHUB_TOKEN }}
+    title: "chore: bump image versions by @${{ github.actor }}"
+    body: |
+      This PR was created by **@${{ github.actor }}** using `flux-helpers`.
+
+      It bumps image versions in:
+      - `${{ inputs.file }}`
+    head: bump/${{ github.actor }}/{{ github.run_id }}
+    base: main
