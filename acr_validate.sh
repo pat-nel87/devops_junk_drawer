1ca10b6ce19b83a07e9a8ac666b0109977813b4f
pat-nel87 <71235856+pat-nel87@users.noreply.github.com>
2025-06-02 11:27:47 -0400

    Update acr_validate.sh

--- acr_validate.sh
+++ acr_validate.sh
@@ -1,4 +1,7 @@
 IMAGE_TAG="mytag"
+ACR_NAME=$(echo "$IMAGE_REF" | cut -d'.' -f1)  # "myregistry"
+REPO_NAME=$(echo "$IMAGE_REF" | cut -d'/' -f2 | cut -d':' -f1)  # "myapp"
+IMAGE_TAG=$(echo "$IMAGE_REF" | cut -d':' -f2)  # "mytag"
 
 if az acr repository show-tags --name myregistry --repository myapp --query "[?@=='$IMAGE_TAG']" -o tsv | grep -q "$IMAGE_TAG"; then
   echo "Image tag '$IMAGE_TAG' exists."
